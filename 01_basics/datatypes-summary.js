ebdf6b57f0025c6a5a045d9771bfe0788d7fb7a8
kanzaderenuka43 <132345261+kanzaderenuka43@users.noreply.github.com>
2023-10-17 07:17:53 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,15 +1,15 @@
 // Primitive
 // 7 types:String,number,Boolean,null,undefined,Symbol,BigInt
 
-const score = 100
-const scoreValue = 100.3
-const isoggedIn = false
-const outsideTemp = null
-let userEmail;
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id === anotherId)
-const bigNum = 47587878675876n
+// const score = 100
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId)
+// const bigNum = 47587878675876n
 
 
 
@@ -28,3 +28,4 @@ let myObj = {
     console.log(typeof myFunction)
     
 
+ 
