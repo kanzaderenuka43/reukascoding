abffcd450ef62384f993e07513983d0b5c22d8b9
kanzaderenuka43 <132345261+kanzaderenuka43@users.noreply.github.com>
2023-10-17 06:44:14 +0000

    comparison operation

---
+++ 01_basics/03_compare.js
@@ -0,0 +1,48 @@
+// let value = 3
+// let neg = -value
+// console.log(neg)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+// let  str1 = "hello"
+// let str2 = "renuka"
+// let str3 = str1 + str2
+// console.log(str3)
+// console.log("2" + 3)
+// console.log("2" + 3 + 4)
+// console.log(2 + 3 + "4")
+// console.log(+true)
+// let num1,num2,num3
+// num1 = num2 = num3 = 2 + 3
+// let gameCounter = 100
+// ++gameCounter
+// console.log(gameCounter)
+// prefix and postfix
+// let x = 3;
+// const y = x++;
+// console.log(`x:${x}, y:${y}`);
+// let a = 3;
+// const b = ++a;
+// console.log(`a:${a}, b:${b}`)
+// console.log(2<6)
+// console.log(3<=2)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log("2" > 1)
+// console.log("02" > 1)
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+
+// ===
+console.log("2" === 2)
+
+
+
+
