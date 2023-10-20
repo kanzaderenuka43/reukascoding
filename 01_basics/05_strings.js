b79284bdb14f5c10671fe42a5cf461e240953470
kanzaderenuka43 <132345261+kanzaderenuka43@users.noreply.github.com>
2023-10-20 14:21:59 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name = "hitesh"
+const repoCount = 50
+// console.log(name + repoCount + "Value")
+// console.log(`hello my name is ${name}, and my repo count is ${repoCount}`)
+const gameName = new String('renukapatil-rajesh-tanmay')
+console.log(gameName[1])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.slice(4,21))
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('k'))
+const newString = gameName.substring(0,4)
+console.log(newString)
+const anotherString = gameName.slice(-23,4)
+console.log(anotherString)
+const newStringOne = "  hgjhjghjhj  "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+const url = "https://www.w3schools.com/js/tryit.asp?filename=tryjs%20string"
+// console.log(url.replace('%20','_'))
+// console.log(url.includes(tryjs))
+
+console.log(gameName.split('-'))
