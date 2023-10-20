77247e938d3352569abcdf1f375a3045b115aaa1
kanzaderenuka43 <132345261+kanzaderenuka43@users.noreply.github.com>
2023-10-20 15:33:19 +0000

    math and num

--- 01_basics/06_math_and_num.js
+++ 01_basics/06_math_and_num.js
@@ -1,4 +1,29 @@
-const score = 400
-console.log(score)
-const bal = new Number(100)
-console.log(bal)
+// const score = 400
+// console.log(score)
+// const bal = new Number(100)
+// console.log(bal.toString().length)
+// console.log(bal.toFixed(1))
+// const otherNumber = 123.98732
+// console.log(otherNumber.toPrecision(4))
+// const hun = 10000000
+// console.log(hun.toLocaleString('en-IN'))
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.55))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.1))
+// console.log(Math.min(33,2,12))
+// console.log(Math.max(33,2,44))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+console.log((Math.random()*10) + 1)
+console.log(Math.random())
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*10) + 1)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
+
+
+
