7fcc0e795a75405e77e730aba450c1a8a350f09e
kanzaderenuka43 <132345261+kanzaderenuka43@users.noreply.github.com>
2023-10-28 17:21:52 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,45 @@
+// var c = 300
+let a = 200
+if (true){
+    let a = 10
+    const b = 20
+    // console.log("Inner: ", a)
+}
+//  console.log(a)
+// console.log(b)
+// console.log(c)
+
+function one(){
+    const uname = "renuka"
+    function two(){
+        const website = "youtube"
+        console.log(uname);
+
+    }
+    // console.log(website);
+
+// two()
+} 
+// one()
+if(true)
+{
+const uname = "renuka"
+if(uname === "renuka")
+{
+const website = " youtube"
+// console.log(uname + website);
+}
+// console.log(website);
+
+}
+// console.log(uname)
+// ++++++++++++++++ interesting ++++++++++++++++
+
+// console.log(addone(5))
+function addone(num2){
+    return num2 + 1
+}
+addTwo(3)
+const addTwo = function(num){
+    return num + 1
+}
