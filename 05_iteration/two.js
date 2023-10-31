b0be3537802a65953f5fcafe75c2b061ae4fec41
kanzaderenuka43 <132345261+kanzaderenuka43@users.noreply.github.com>
2023-10-31 07:03:30 +0000

    do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,23 @@
+// let index = 0
+// while (index <= 10 ){
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+
+// }
+// index = index + 2(loop infinite)
+
+// let myArr = ['flash', "batman", "superman"]
+// let arr = 0
+// while (arr < myArr.length){
+//     console.log(`value is ${myArr[arr]}`);
+//     arr = arr + 2
+// }
+
+
+let score = 1
+do{
+    console.log(`score is ${score}`);
+    score++
+}
+while (score < 10) ;
+    
